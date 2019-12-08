182d28309c1adb6a0c82deb35bcc46347282f4ad
Vinicius Flores <viniciusflores379@gmail.com>
2019-12-08 17:48:38 -0300

    :sparkles: start text robot

---
+++ robots/text.js
@@ -0,0 +1,65 @@
+const algorithmia = require("algorithmia");
+const algorithmiaApiKey = require("../credentials/algorithmia.json").apiKey;
+const sentenceBoundaryDetection = require("sbd");
+
+async function robot(content) {
+  await fetchContentFromWikipedia(content);
+  sanitizeContent(content);
+  breakContentIntoSentences(content);
+
+  async function fetchContentFromWikipedia(content) {
+    const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey);
+    const wikipediaAugorithm = algorithmiaAuthenticated.algo(
+      "web/WikipediaParser/0.1.2"
+    );
+    const wikipediaResponse = await wikipediaAugorithm.pipe(content.searchTerm);
+    const wikipediaContent = wikipediaResponse.get();
+
+    content.sourceContentOriginal = wikipediaContent.content;
+  }
+
+  function sanitizeContent(content) {
+    const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(
+      content.sourceContentOriginal
+    );
+    const withoutDatesInParentheses = removeDatesInParentheses(
+      withoutBlankLinesAndMarkdown
+    );
+
+    content.sourceContentSanitized = withoutDatesInParentheses;
+
+    function removeBlankLinesAndMarkdown(text) {
+      const allLines = text.split("\n");
+
+      const withoutBlankLinesAndMarkdown = allLines.filter(line => {
+        if (line.trim().length === 0 || line.trim().startsWith("=")) {
+          return false;
+        }
+
+        return true;
+      });
+
+      return withoutBlankLinesAndMarkdown.join(" ");
+    }
+  }
+
+  function removeDatesInParentheses(text) {
+    return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, "").replace(/  /g, " ");
+  }
+
+  function breakContentIntoSentences(content) {
+    content.sentences = []
+    
+    const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+    sentences.forEach((sentence) => {
+        content.sentences.push({
+            text: sentence,
+            keywords: [],
+            images : []
+        })
+    })
+
+  }
+}
+
+module.exports = robot;
