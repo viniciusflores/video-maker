f566bb5e031455775ed2ccab516dcd0470db2994
Vinicius Flores <viniciusflores379@gmail.com>
2019-12-09 21:50:23 -0300

    :sparkles: add Download images

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ const robots = {
 }
 
 async function start() {
-    robots.input()
-    await robots.text()
+    //robots.input()
+    //await robots.text()
     await robots.image()
 
-    const content = robots.state.load()
-    console.dir(content, {
-        depth: null
-    })
+    //const content = robots.state.load()
+    //console.dir(content, { depth: null })
 }
 
 start()
--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader')
 const google = require('googleapis').google
 const customSearch = google.customsearch('v1')
 const state = require('./state.js')
@@ -6,10 +7,10 @@ const googleSearchCredentials = require('../credentials/google-search.json')
 
 async function robot() {
     const content = state.load()
+    await downloadAllImages(content)
+    //await fetchImagesOfAllSentences(content)
 
-    await fetchImagesOfAllSentences(content)
-
-    state.save(content)
+    //state.save(content)
 
     async function fetchImagesOfAllSentences(content) {
         for (const sentence of content.sentences) {
@@ -34,6 +35,38 @@ async function robot() {
         })
 
         return imagesUrl
+    }
+
+    async function downloadAllImages(content) {
+        content.downloadAllImages = []
+
+        for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
+            const images = content.sentences[sentenceIndex].images
+
+            for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+                const imageUrl = images[imageIndex]
+
+                try {
+                    if (content.downloadAllImages.includes(imageUrl)){
+                        throw new Error('Imagem já foi baixada')
+                    }
+
+                    await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`)
+                    content.downloadAllImages.push(imageUrl)
+                    console.log(`> [${sentenceIndex}] [${imageIndex}] Baixou a imagem com sucesso: ${imageUrl}`)
+                    break
+                } catch (error) {
+                    console.log(`> [${sentenceIndex}] [${imageIndex}] Erro ao baixar (${imageUrl}): ${error}`)
+                }
+            }
+        }
+    }
+
+    async function downloadAndSave(url, fileName){
+        return imageDownloader.image({
+            url, url,
+            dest: `./content/${fileName}`
+        })
     }
 }
 
