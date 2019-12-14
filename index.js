a498ce05a4263d5ce698217312ed772e61187cef
Vinicius Flores <viniciusflores379@gmail.com>
2019-12-14 13:32:54 -0300

    :sparkles: add robot youtube

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const robots = {
     input: require('./robots/input.js'),
     state: require('./robots/state.js'),
     image: require('./robots/image.js'),
-    video: require('./robots/video.js')
+    video: require('./robots/video.js'),
+    youtube: require('./robots/youtube.js')
 }
 
 async function start() {
@@ -11,6 +12,7 @@ async function start() {
     await robots.text()
     await robots.image()
     await robots.video()
+    await robots.youtube()
 }
 
 start()
