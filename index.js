76e76c62685d61cd889f03cd59a013fe69c6e1a6
Vinicius Flores <viniciusflores379@gmail.com>
2019-12-14 12:37:43 -0300

    :sparkles: render video aftereffects

--- index.js
+++ index.js
@@ -2,18 +2,15 @@ const robots = {
     text: require('./robots/text.js'),
     input: require('./robots/input.js'),
     state: require('./robots/state.js'),
-    image: require('./robots/image.js')
+    image: require('./robots/image.js'),
+    video: require('./robots/video.js')
 }
 
 async function start() {
     robots.input()
     await robots.text()
     await robots.image()
-
-    const content = robots.state.load()
-    console.dir(content, {
-        depth: null
-    })
+    await robots.video()
 }
 
 start()
